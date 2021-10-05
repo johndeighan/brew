e61c11197671a97925004af48c1e94dda75a433d
John Deighan <john.deighan@protonmail.com>
2021-10-05 09:55:09 -0400

    enable file watching

--- bin/cielo.coffee
+++ bin/cielo.coffee
@@ -1,182 +1,116 @@
 `#!/usr/bin/env node
 `
 import {strict as assert} from 'assert'
-import {existsSync, lstatSync} from 'fs'
-import {parse} from 'path'
-import parseArgs from 'minimist';
+import parseArgs from 'minimist'
+import {parse as parsePath} from 'path'
+import chokidar from 'chokidar'
 
-import {undef, pass, croak, words} from '@jdeighan/coffee-utils'
+import {undef, croak, words} from '@jdeighan/coffee-utils'
 import {log} from '@jdeighan/coffee-utils/log'
 import {
-	slurp, barf, getFullPath, forEachFile, withExt, mkpath,
+	slurp, barf, forEachFile, withExt, mkpath,
 	} from '@jdeighan/coffee-utils/fs'
-import {
-	setDebugging, debugging, debug,
-	} from '@jdeighan/coffee-utils/debug'
+import {setDebugging, debug} from '@jdeighan/coffee-utils/debug'
 import {untabify} from '@jdeighan/coffee-utils/indent'
 import {loadEnvFrom} from '@jdeighan/env'
 import {starbucks} from '@jdeighan/starbucks'
 import {brewCielo} from './brewCielo.js'
 
 ###
-	cielo [-c | -j | -s | -h | -d | -D ] (<dir> | <file>)+
+	cielo [-h | -n | -d | -D ]
 ###
 
-# --- default settings
-doCieloToCoffee = false
-doCieloToJS = true
-doStarbucks = true
-doLog = false
+doLog = false    # log files processed
+doWatch = true   # turn off with -n
 
-# ---------------------------------------------------------------------------
-
-lLoadedEnvPaths = []
-
-loadEnvironment = (dir) ->
-
-	if not lLoadedEnvPaths.includes(dir)
-		loadEnvFrom(dir)
-		lLoadedEnvPaths.push dir
-		if debugging
-			dumpDirs()
-	return
+dirRoot = process.cwd()
 
 # ---------------------------------------------------------------------------
 
 main = () ->
 
+	handleCmdArgs()
+	loadEnvFrom dirRoot
+
+	log "...processing #{dirRoot}"
+	watcher = chokidar.watch(dirRoot, {
+		persistent: doWatch,
+		})
+	watcher.on 'all', (event, path) ->
+		if path.match(/node_modules/)
+			return
+		if lMatches = path.match(/\.(cielo|starbucks)$/)
+			ext = ".#{lMatches[1]}"
+			if doLog
+				console.log event, path
+			if ext == '.cielo'
+				brewCieloFile(path)
+			else if ext == 'starbucks'
+				brewStarbucksFile(path)
+
+	return
+
+# ---------------------------------------------------------------------------
+
+brewCieloFile = (path) ->
+
+	code = brewCielo(slurp(path), 'js')
+	output code, path, '.js'
+	return
+
+# ---------------------------------------------------------------------------
+
+brewStarbucksFile = (path) ->
+
+	hParsed = parsePath(path)
+	hOptions = {
+		content: slurp(path),
+		filename: hParsed.base,
+		}
+	code = starbucks(hOptions).code
+	output code, path, '.svelte'
+	return
+
+# ---------------------------------------------------------------------------
+
+output = (code, inpath, outExt) ->
+
+	outpath = withExt(inpath, outExt)
+	barf outpath, code
+	if doLog
+		log "   #{inpath} => #{outExt}"
+	return
+
+# ---------------------------------------------------------------------------
+
+handleCmdArgs = () ->
+
 	hArgs = parseArgs(process.argv.slice(2), {
-			boolean: words('c j s h d D'),
+			boolean: words('h n d D'),
 			unknown: (opt) ->
 				return true
 			})
 
 	# --- Handle request for help
 	if hArgs.h
-		console.log "cielo dir or file"
-		console.log "   -c convert *.cielo to *.coffee files"
-		console.log "   -j convert *.cielo to *.js files"
-		console.log "   -s convert *.starbucks to *.svelte files"
-		console.log "   -d print every file processed"
-		console.log "   -D turn on debugging (a lot of output!)"
-		console.log "   -h help"
+		log "cielo"
+		log "   -h help"
+		log "   -n process files, don't watch for changes"
+		log "   -d print every file processed"
+		log "   -D turn on debugging (a lot of output!)"
 		process.exit()
 
 	if hArgs.d
+		log "debugging on"
 		doLog = true
-		log "hArgs", hArgs
 
 	if hArgs.D
+		log "extensive debugging on"
 		setDebugging true
 
-	# --- If neither -c, -j or -s are set, we'll process both types of files
-	#     But that only applies to directories - starbucks and cielo files
-	#     appearing on the command line are always processed
-
-	if hArgs.c || hArgs.j || hArgs.s
-		doCieloToCoffee = hArgs.c
-		doCieloToJS = hArgs.j
-		doStarbucks = hArgs.s
-
-	# --- Resolve paths, checking that they all exist
-	lPaths = []
-	for orgPath in hArgs._
-		debug "cielo(): orgPath = '#{orgPath}'"
-		path = getFullPath(orgPath)  # resolve relative paths
-		debug "resolved to '#{path}'"
-
-		# --- may be a file or a directory
-		assert existsSync(path), "'#{path}' does not exist"
-		lPaths.push path
-
-	for path in lPaths
-		ent = lstatSync(path)
-		if ent.isFile()
-			{dir, ext, base} = parse(path)
-
-			# --- Load environment from directory containing source file
-			loadEnvironment dir
-
-			if (ext == '.starbucks')
-				brew(dir, base, '.starbucks', '.svelte')
-			else if (ext == '.cielo')
-				if doCieloToCoffee
-					brew(dir, base, '.cielo', '.coffee')
-				else
-					brew(dir, base, '.cielo', '.js')
-			else
-				croak "Can't brew #{base}"
-		else if ent.isDirectory()
-
-			# --- Load environment from given directory
-			loadEnvironment path
-			brewDirectory(path)
-	return
-
-# ---------------------------------------------------------------------------
-
-brewDirectory = (dir) ->
-
-	debug "brew files in dir '#{dir}'"
-
-	if doCieloToCoffee
-		cbCieloToCoffee = (base, dir, level) ->
-			brew(dir, base, '.cielo', '.coffee')
-			return
-
-		forEachFile(dir, cbCieloToCoffee, /\.cielo$/)
-
-	if doCieloToJS
-		cbCieloToJS = (base, dir, level) ->
-			brew(dir, base, '.cielo', '.js')
-			return
-
-		forEachFile(dir, cbCieloToJS, /\.cielo$/)
-
-	if doStarbucks
-		cbStarbucks = (base, dir, level) ->
-			brew(dir, base, '.starbucks', '.svelte')
-			return
-
-		forEachFile(dir, cbStarbucks, /\.starbucks$/)
-	return
-
-# ---------------------------------------------------------------------------
-
-brew = (dir, filename, srcExt, destExt) ->
-
-	path = mkpath(dir, filename)
-	content = slurp(path)
-	if srcExt == '.starbucks'
-		assert destExt == '.svelte', "brew(): Bad dest ext #{destExt}"
-		result = starbucks({content, filename}).code
-	else if srcExt == '.cielo'
-		if destExt == '.coffee'
-			result = brewCielo(content, 'coffee')
-		else if destExt == '.js'
-			result = brewCielo(content, 'js')
-		else
-			croak "brew(): Unknown dest extension: #{destExt}"
-	else
-		croak "brew(): Unknown source extension: #{srcExt}"
-
-	outpath = withExt(path, destExt)
-	barf outpath, untabify(result)
-	debug "BREW: #{path} -> #{outpath}"
-	if doLog
-		log dir
-		log "   #{base} => #{withExt(base, outExt)}"
-	return
-
-# ---------------------------------------------------------------------------
-
-dumpDirs = () ->
-
-	# --- Print out names of defined directories
-	for key,value of process.env
-		if (key.indexOf('DIR_') == 0) || (key.indexOf('dir_') == 0)
-			log "#{key} = #{value}"
-	return
+	if hArgs.n
+		log "not watching for changes"
+		doWatch = false
 
 # ---------------------------------------------------------------------------
 
--- bin/cielo.js
+++ bin/cielo.js
@@ -1,25 +1,24 @@
 #!/usr/bin/env node
 ;
-var brew, brewDirectory, doCieloToCoffee, doCieloToJS, doLog, doStarbucks, dumpDirs, lLoadedEnvPaths, loadEnvironment, main;
+/*
+	cielo [-h | -n | -d | -D ]
+*/
+var brewCieloFile, brewStarbucksFile, dirRoot, doLog, doWatch, handleCmdArgs, main, output;
 
 import {
   strict as assert
 } from 'assert';
 
-import {
-  existsSync,
-  lstatSync
-} from 'fs';
-
-import {
-  parse
-} from 'path';
-
 import parseArgs from 'minimist';
 
 import {
+  parse as parsePath
+} from 'path';
+
+import chokidar from 'chokidar';
+
+import {
   undef,
-  pass,
   croak,
   words
 } from '@jdeighan/coffee-utils';
@@ -31,7 +30,6 @@ import {
 import {
   slurp,
   barf,
-  getFullPath,
   forEachFile,
   withExt,
   mkpath
@@ -39,7 +37,6 @@ import {
 
 import {
   setDebugging,
-  debugging,
   debug
 } from '@jdeighan/coffee-utils/debug';
 
@@ -59,166 +56,98 @@ import {
   brewCielo
 } from './brewCielo.js';
 
-/*
-	cielo [-c | -j | -s | -h | -d | -D ] (<dir> | <file>)+
-*/
-// --- default settings
-doCieloToCoffee = false;
+doLog = false; // log files processed
 
-doCieloToJS = true;
+doWatch = true; // turn off with -n
 
-doStarbucks = true;
-
-doLog = false;
+dirRoot = process.cwd();
 
 // ---------------------------------------------------------------------------
-lLoadedEnvPaths = [];
-
-loadEnvironment = function(dir) {
-  if (!lLoadedEnvPaths.includes(dir)) {
-    loadEnvFrom(dir);
-    lLoadedEnvPaths.push(dir);
-    if (debugging) {
-      dumpDirs();
+main = function() {
+  var watcher;
+  handleCmdArgs();
+  loadEnvFrom(dirRoot);
+  log(`...processing ${dirRoot}`);
+  watcher = chokidar.watch(dirRoot, {
+    persistent: doWatch
+  });
+  watcher.on('all', function(event, path) {
+    var ext, lMatches;
+    if (path.match(/node_modules/)) {
+      return;
     }
+    if (lMatches = path.match(/\.(cielo|starbucks)$/)) {
+      ext = `.${lMatches[1]}`;
+      if (doLog) {
+        console.log(event, path);
+      }
+      if (ext === '.cielo') {
+        return brewCieloFile(path);
+      } else if (ext === 'starbucks') {
+        return brewStarbucksFile(path);
+      }
+    }
+  });
+};
+
+// ---------------------------------------------------------------------------
+brewCieloFile = function(path) {
+  var code;
+  code = brewCielo(slurp(path), 'js');
+  output(code, path, '.js');
+};
+
+// ---------------------------------------------------------------------------
+brewStarbucksFile = function(path) {
+  var code, hOptions, hParsed;
+  hParsed = parsePath(path);
+  hOptions = {
+    content: slurp(path),
+    filename: hParsed.base
+  };
+  code = starbucks(hOptions).code;
+  output(code, path, '.svelte');
+};
+
+// ---------------------------------------------------------------------------
+output = function(code, inpath, outExt) {
+  var outpath;
+  outpath = withExt(inpath, outExt);
+  barf(outpath, code);
+  if (doLog) {
+    log(`   ${inpath} => ${outExt}`);
   }
 };
 
 // ---------------------------------------------------------------------------
-main = function() {
-  var base, dir, ent, ext, hArgs, i, j, lPaths, len, len1, orgPath, path, ref;
+handleCmdArgs = function() {
+  var hArgs;
   hArgs = parseArgs(process.argv.slice(2), {
-    boolean: words('c j s h d D'),
+    boolean: words('h n d D'),
     unknown: function(opt) {
       return true;
     }
   });
   // --- Handle request for help
   if (hArgs.h) {
-    console.log("cielo dir or file");
-    console.log("   -c convert *.cielo to *.coffee files");
-    console.log("   -j convert *.cielo to *.js files");
-    console.log("   -s convert *.starbucks to *.svelte files");
-    console.log("   -d print every file processed");
-    console.log("   -D turn on debugging (a lot of output!)");
-    console.log("   -h help");
+    log("cielo");
+    log("   -h help");
+    log("   -n process files, don't watch for changes");
+    log("   -d print every file processed");
+    log("   -D turn on debugging (a lot of output!)");
     process.exit();
   }
   if (hArgs.d) {
+    log("debugging on");
     doLog = true;
-    log("hArgs", hArgs);
   }
   if (hArgs.D) {
+    log("extensive debugging on");
     setDebugging(true);
   }
-  // --- If neither -c, -j or -s are set, we'll process both types of files
-  //     But that only applies to directories - starbucks and cielo files
-  //     appearing on the command line are always processed
-  if (hArgs.c || hArgs.j || hArgs.s) {
-    doCieloToCoffee = hArgs.c;
-    doCieloToJS = hArgs.j;
-    doStarbucks = hArgs.s;
-  }
-  // --- Resolve paths, checking that they all exist
-  lPaths = [];
-  ref = hArgs._;
-  for (i = 0, len = ref.length; i < len; i++) {
-    orgPath = ref[i];
-    debug(`cielo(): orgPath = '${orgPath}'`);
-    path = getFullPath(orgPath); // resolve relative paths
-    debug(`resolved to '${path}'`);
-    // --- may be a file or a directory
-    assert(existsSync(path), `'${path}' does not exist`);
-    lPaths.push(path);
-  }
-  for (j = 0, len1 = lPaths.length; j < len1; j++) {
-    path = lPaths[j];
-    ent = lstatSync(path);
-    if (ent.isFile()) {
-      ({dir, ext, base} = parse(path));
-      // --- Load environment from directory containing source file
-      loadEnvironment(dir);
-      if (ext === '.starbucks') {
-        brew(dir, base, '.starbucks', '.svelte');
-      } else if (ext === '.cielo') {
-        if (doCieloToCoffee) {
-          brew(dir, base, '.cielo', '.coffee');
-        } else {
-          brew(dir, base, '.cielo', '.js');
-        }
-      } else {
-        croak(`Can't brew ${base}`);
-      }
-    } else if (ent.isDirectory()) {
-      // --- Load environment from given directory
-      loadEnvironment(path);
-      brewDirectory(path);
-    }
-  }
-};
-
-// ---------------------------------------------------------------------------
-brewDirectory = function(dir) {
-  var cbCieloToCoffee, cbCieloToJS, cbStarbucks;
-  debug(`brew files in dir '${dir}'`);
-  if (doCieloToCoffee) {
-    cbCieloToCoffee = function(base, dir, level) {
-      brew(dir, base, '.cielo', '.coffee');
-    };
-    forEachFile(dir, cbCieloToCoffee, /\.cielo$/);
-  }
-  if (doCieloToJS) {
-    cbCieloToJS = function(base, dir, level) {
-      brew(dir, base, '.cielo', '.js');
-    };
-    forEachFile(dir, cbCieloToJS, /\.cielo$/);
-  }
-  if (doStarbucks) {
-    cbStarbucks = function(base, dir, level) {
-      brew(dir, base, '.starbucks', '.svelte');
-    };
-    forEachFile(dir, cbStarbucks, /\.starbucks$/);
-  }
-};
-
-// ---------------------------------------------------------------------------
-brew = function(dir, filename, srcExt, destExt) {
-  var content, outpath, path, result;
-  path = mkpath(dir, filename);
-  content = slurp(path);
-  if (srcExt === '.starbucks') {
-    assert(destExt === '.svelte', `brew(): Bad dest ext ${destExt}`);
-    result = starbucks({content, filename}).code;
-  } else if (srcExt === '.cielo') {
-    if (destExt === '.coffee') {
-      result = brewCielo(content, 'coffee');
-    } else if (destExt === '.js') {
-      result = brewCielo(content, 'js');
-    } else {
-      croak(`brew(): Unknown dest extension: ${destExt}`);
-    }
-  } else {
-    croak(`brew(): Unknown source extension: ${srcExt}`);
-  }
-  outpath = withExt(path, destExt);
-  barf(outpath, untabify(result));
-  debug(`BREW: ${path} -> ${outpath}`);
-  if (doLog) {
-    log(dir);
-    log(`   ${base} => ${withExt(base, outExt)}`);
-  }
-};
-
-// ---------------------------------------------------------------------------
-dumpDirs = function() {
-  var key, ref, value;
-  ref = process.env;
-  // --- Print out names of defined directories
-  for (key in ref) {
-    value = ref[key];
-    if ((key.indexOf('DIR_') === 0) || (key.indexOf('dir_') === 0)) {
-      log(`${key} = ${value}`);
-    }
+  if (hArgs.n) {
+    log("not watching for changes");
+    return doWatch = false;
   }
 };
 
